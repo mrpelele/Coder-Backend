93eff54ddda65a25e17ea0c500c372401686064c
Julián Marega <julianm@enfoke.com>
2021-11-04 10:06:29 -0300

    added api functionalities

--- Functionalities/Constructor.js
+++ Functionalities/Constructor.js
@@ -5,6 +5,14 @@ const readFileAsync = async (route) => {
     return storedData
 }
 
+const writeFileAsync = async (prod,route) => {
+    fs.writeFile(route,JSON.stringify(prod) ,{encoding:'utf-8'},(err) => {
+        if (err) {
+            console.log('no se a podido guardar el objeto')
+        }
+    })
+}
+
 class Container {
 
     constructor (route) {
@@ -40,7 +48,6 @@ class Container {
 
             let objArray = JSON.parse(fileValidator)
             console.log('this is the entire db:')
-            console.log(objArray)
             return objArray
 
         } else {
@@ -48,6 +55,83 @@ class Container {
         }
 
     }
+
+    async saveProd(prod) {
+
+        let finalProd = []
+        
+        let fileValidator = await readFileAsync(this.route)
+        
+        if (fileValidator && fileValidator.length >= 0) {
+
+            finalProd = JSON.parse(fileValidator)
+            prod.id = finalProd.length+1
+            let currentObjId = prod.id
+            finalProd.push(prod)
+            console.log(finalProd)
+            writeFileAsync(finalProd,this.route,currentObjId)
+
+        } else {
+            prod.id = 1
+            let currentObjId = JSON.stringify(prod.id)
+            finalProd.push(prod)
+            writeFileAsync(finalProd,this.route,currentObjId)
+        }
+
+    }
+
+    async updateProd(id,prod) {
+
+        let fileValidator = await readFileAsync(this.route)
+
+        if (fileValidator) {
+
+            let ValidatedObj = JSON.parse(fileValidator)
+            let filteredProduct = ValidatedObj.filter(element => element.id !== id )
+            filteredProduct.push(prod)
+
+            if (filteredProduct) {
+
+                writeFileAsync(filteredProduct,this.route,id)
+
+            } else {
+
+                console.log('error updating product')
+
+            }
+
+        } else {
+
+            console.log('product not found')
+
+        }
+    } 
+
+    async deleteById(id) {
+
+        let fileValidator = await readFileAsync(this.route)
+
+        if (fileValidator) {
+
+            let ValidatedObj = JSON.parse(fileValidator)
+            let filteredProduct = ValidatedObj.filter(element => element.id !== id )
+
+            if (filteredProduct) {
+
+                writeFileAsync(filteredProduct,this.route)
+                console.log('product deleted')
+
+            } else {
+
+                console.log('error deleting product')
+
+            }
+
+        }
+    } 
+
+
+
 
 }
 
--- index.js
+++ index.js
@@ -1,38 +1,73 @@
 const fs = require('fs')
 const express = require('express')
+const bp = require('body-parser')
+const { Router } = express
 const Container = require('./Functionalities/Constructor')
 
 const app = express()
+const router = Router()
+app.use(bp.json())
+app.use(bp.urlencoded({extended: true}))
 
 const container = new Container ('./data/db.txt')
        
-    app.get("/", (req,res) => {
+    router.get("/", (req,res) => {
 
-        res.send('main')
+        res.send('main page')
 
     })
 
-    app.get("/hola", (req,res) => {
+    router.get("/hola", (req,res) => {
 
         res.send('hola')
 
     })
 
-    app.get("/product", async (req,res) => {
+    router.get("/product", async (req,res) => {
 
         res.send(await container.getAll());
 
     })
 
-    app.get("/productRandom", async (req,res) => {
+    router.get("/product/:id", async (req,res) => {
 
-        
+        res.send(await container.getById(JSON.parse(req.params.id)))
 
-        randomNum = Math.floor(Math.random() * 3) + 1
+    })
 
+    router.get("/productR", async (req,res) => {
+
+        randomNum = Math.floor(Math.random() * 5) + 1
         res.send(await container.getById(randomNum))
 
     })
+
+    router.put("/product/:id" , async (req,res) => {
+
+        const {name,price} = req.body
+        const id = JSON.parse(req.params.id)
+        const obj = {name,price,id}
+        res.send(await container.updateProd(id,obj))
+        console.log('updated!')
+
+    })
+
+    router.post("/product/add", async (req,res) => {
+
+        const {name,price} = req.body
+        const obj = {name,price}
+        res.send(await container.saveProd(obj))
+        console.log('saved')
+
+    })
+
+    router.delete("/product/:id", async (req,res) => {
+
+        res.send(await container.deleteById(JSON.parse(req.params.id)))
+
+    })
+
+app.use('/api',router)
 
 app.listen(3007, () => {
 
